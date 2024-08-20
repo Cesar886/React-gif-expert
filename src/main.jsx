58a12409fa21bd1ecda3162cbecb65ab9ad37611
Daniel <cesar4herrera@gmail.com>
2024-08-20 10:47:19 -0600

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 import GiftExpertApp from './Components/GiftExpertApp';
